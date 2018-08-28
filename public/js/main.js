8317cca5023c8ac962fde119cb443385a523b4bc
joaoppmadureira@gmail.com <joaoppmadureira@gmail.com>
2018-08-28 22:33:29 +0100

    first version of game with background

---
+++ public/js/main.js
@@ -0,0 +1,38 @@
+import SpriteSheet from './SpriteSheet.js'
+import { loadImage, loadLevel } from './loaders.js'
+
+function drawBackground(background, context, sprites) {
+    background.ranges.forEach(([x1, x2, y1, y2]) => {
+        for (let x = x1; x < x2; ++x) {
+            for (let y = y1; y < y2; ++y) {
+                sprites.drawTile(background.tile, context, x, y);
+            }
+        }
+    });
+}
+
+function loadBackgroundSprites() {
+    return loadImage('/img/tiles/experimental/forest_tiles.png')
+    .then(image => {
+        const sprites = new SpriteSheet(image, 16, 16);
+        sprites.define('ground', 0, 0);
+        return sprites;
+    });
+}
+
+const canvas = document.getElementById('screen');
+const context = canvas.getContext('2d');
+
+Promise.all([
+    loadBackgroundSprites(),
+    loadLevel('1-1'),
+])
+.then(([sprites, level]) => {
+    level.backgrounds.forEach(background => {
+        drawBackground(level.backgrounds[0], context, sprites)
+    })
+});
+
+
+
+
